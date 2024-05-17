4b556c7bab05a4bf27a7f9f832da49a85ba0ca1a
jospecarlos <josecarlosf004@gmail.com>
2024-05-17 20:58:59 -0300

    meu primeiro commit

---
+++ script.js
@@ -0,0 +1,37 @@
+window.alert ('minha primeira mensagem');
+window.alert ('qual seu nome')
+window.confirme('esta gostando')
+window.pronpt('qual e seu nome')
+   
+/*
+    objetivo 1 - quando o usuario clicar no botão de de mostrar mais deve abrir os projetos que estão escondidos no html
+
+    passo 1 - pegar o botao e mostrar mais no js pra poder verificar quando o usuário clicar em cima dele
+
+    passo 2 - indentificar o clique no botao 
+
+    passo 3 - adicionar a classe " ativo" nos projetos escondidos 
+    
+    obijetivo 2 - esconder o botao de mostrar mais
+    passo 1 - pegar  o botao e esconder
+*/
+
+//objetivo 1 - quando o usuario clicar no botão de de mostrar mais deve abrir os projetos que estão escondidos no html
+
+// passo 1 - pegar o botao e mostrar mais no js pra poder verificar quando o usuário clicar em cima dele
+
+     //caso tenho mais projetos
+
+/*const botaomostrarprojetos = document.querySelector('.btn-mostrar-projeto'));
+
+const projetosinativos = document.querySelector('projetos:not(.ativo)');
+
+botaomostrarprojetos.addEventListener('click', () => {
+    //passo 3 - adicionar a classe " ativo" nos projetos escondidos
+    projetosinativos.foreach(projetosInativo =>{
+        projetosInativo.classlist.add('ativo')
+    })
+   // obijetivo 2 - esconder o botao de mostrar mais
+   //passo 1 - pegar  o botao e esconder
+   botaomostrarprojetos.classList.add("remover")
+})*/
